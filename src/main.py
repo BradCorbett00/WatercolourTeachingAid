be10bbcddc7d0a959780b7fbf1265c51185722f1
Brad Corbett <45594006+BradCorbett00@users.noreply.github.com>
2022-04-28 21:17:35 +0100

    GUI now reduces image to a display where they can be seen, and reduce image size was refactored in a way that reduces code duplication

--- src/main.py
+++ src/main.py
@@ -9,6 +9,8 @@ import cv2 as cv
 import numpy as np
 import PySimpleGUI as gui
 import os.path
+from PIL import Image
+from PIL import ImageTk
 
 
 def quant_image(image, k):
@@ -80,16 +82,38 @@ def retrieve_value_study(image):
     return image_whites_and_lights, image_whites_lights_and_mids, image_whites_lights_mids_and_darks, value_study_image
 
 
-def resize_image(img, scale):
-    new_width = int(img.shape[1] * scale / 100)
-    new_height = int(img.shape[0] * scale / 100)
-    new_size = (1280, 720)
+def resize_image(img, target_dimension): #Works with an OpenCV image
+    width = int(img.shape[1])
+    height = int(img.shape[0])
+
+    new_size = reduce_resolution(height, width, target_dimension)
+
     new_img = cv.resize(img, new_size, interpolation=cv.INTER_AREA)
+
     return new_img
 
 
-def process_image(img, scale):
-    img = resize_image(img, 50)
+def cut_image_size(image_to_shrink, target_dimension) -> tuple[int, int]: #Works with a PIL image
+    width = int(image_to_shrink.size[0])
+    height = int(image_to_shrink.size[1])
+
+    new_size = reduce_resolution(height, width, target_dimension)
+
+    return new_size
+
+
+def reduce_resolution(height, width, target_dimension):
+    image_below_target: bool = ((width < target_dimension) and (height < target_dimension))
+    while not image_below_target:
+        width = int(width * 0.75)
+        height = int(height * 0.75)
+        image_below_target = ((width <= target_dimension) and (height <= target_dimension))
+    new_size = (width, height)
+    return new_size
+
+
+def process_image(img, target_dimension):
+    img = resize_image(img, target_dimension)
 
     img = cv.bilateralFilter(img, 35, 75, 75)
 
@@ -119,7 +143,7 @@ def create_ui():
     image_viewer_column = [
         [gui.Text("Choose an image from list on left:")],
         [gui.Text(size=(40, 1), key="-TOUT-")],
-        [gui.Image(key="-IMAGE-")],
+        [gui.Image(key="-IMAGE-")]
     ]
 
     layout = [
@@ -130,7 +154,7 @@ def create_ui():
         ]
     ]
 
-    display_window = gui.Window("Tech-Nicolour", layout)
+    display_window = gui.Window("Tech-Nicolour", layout, resizable=True)
 
     while True:
         event, values = display_window.read()
@@ -152,29 +176,33 @@ def create_ui():
             ]
             display_window["-FILE LIST-"].update(file_names)
         elif event == "-FILE LIST-":  # A file was chosen from the listbox
-            try:
-                filename = os.path.join(
-                    values["-FOLDER-"], values["-FILE LIST-"][0]
-                )
-                display_window["-TOUT-"].update(filename)
-                display_window["-IMAGE-"].update(filename=filename)
-            except:
-                pass
+            filename = os.path.join(
+                values["-FOLDER-"], values["-FILE LIST-"][0]
+            )
+            im = Image.open(filename)
+            from PIL.Image import Resampling
+
+            size = cut_image_size(im, 720)
+
+            im = im.resize(size, resample=Resampling.BICUBIC)
+
+            image = ImageTk.PhotoImage(image=im)
+
+            display_window["-TOUT-"].update(filename)
+            display_window["-IMAGE-"].update(data=image)
 
     display_window.close()
 
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    create_ui()
+    # create_ui()
     storage_location = 'resources\\produced_images\\'
-
-    startTime = time.time()
     img = cv.imread('resources\\test_images\\Glacier.jpg')
-    readTime = time.time() - startTime
 
     img_name = "Glacier"
 
-    value_study_test = process_image(img, 50)
+    value_study_test = process_image(img, 1280)
 
     value_study_lights = cv.cvtColor(value_study_test[0], cv.COLOR_HSV2BGR)
     value_study_mids = cv.cvtColor(value_study_test[1], cv.COLOR_HSV2BGR)
@@ -182,8 +210,8 @@ if __name__ == '__main__':
     value_study_test_full = cv.cvtColor(value_study_test[3], cv.COLOR_HSV2BGR)
     value_study_black_and_white_test = cv.cvtColor(value_study_test_full, cv.COLOR_BGR2GRAY)
 
-    cv.imwrite(storage_location + img_name + '_lightValueStudy.jpg', value_study_lights)
-    cv.imwrite(storage_location + img_name + '_midValueStudy.jpg', value_study_mids)
-    cv.imwrite(storage_location + img_name + '_darkValueStudy.jpg', value_study_darks)
-    cv.imwrite(storage_location + img_name + '_fullValueStudy.jpg', value_study_test_full)
-    cv.imwrite(storage_location + img_name + '_testBW.jpg', value_study_black_and_white_test)
+    cv.imwrite(storage_location + img_name + '_lightValueStudy.png', value_study_lights)
+    cv.imwrite(storage_location + img_name + '_midValueStudy.png', value_study_mids)
+    cv.imwrite(storage_location + img_name + '_darkValueStudy.png', value_study_darks)
+    cv.imwrite(storage_location + img_name + '_fullValueStudy.png', value_study_test_full)
+    cv.imwrite(storage_location + img_name + '_testBW.png', value_study_black_and_white_test)
